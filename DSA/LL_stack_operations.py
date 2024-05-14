0c863408dbc9f4bb5380a05735c1ce627f7d730c
venky3692 <iyervenkatesh92@gmail.com>
2024-05-15 01:04:38 +0530

    Changing dir

---
+++ DSA/LL_stack_operations.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, value = None):
+        self.value = value
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+class Stack:
+    def __init__(self):
+        self.LinkedList = LinkedList()
