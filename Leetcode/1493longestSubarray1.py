2fc9587c854e6ddad58d2b95d2a370f1cef6d2b7
venky3692 <iyervenkatesh92@gmail.com>
2024-05-29 21:45:07 +0530

    Day16

--- Leetcode/1493longestSubarray1.py
+++ Leetcode/1493longestSubarray1.py
@@ -1,12 +1,12 @@
 class Solution:
     def longestSubarray(self,nums):
-        count_0 = 0
+        count_0 = 1
         l = 0
         for i in range(len(nums)):
             count_0 -= nums[i] == 0
 
             if count_0<0:
                 count_0 += nums[l] == 0
-
+                l +=1
         
         return i - l 
