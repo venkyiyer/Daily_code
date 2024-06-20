9e087a989e596eb6f9a76dc503a708c9bed918ab
venky3692 <iyervenkatesh92@gmail.com>
2024-06-20 23:29:00 +0530

    changes in code O(n)

--- Leetcode/1TwoSum.py
+++ Leetcode/1TwoSum.py
@@ -1,10 +1,11 @@
 class solution:
-
     def twoSum(self, nums, target):
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+        dic = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in dic:
+                return i, dic[diff]
+            dic[nums[i]] = i
 
 obj = solution()
-print(obj.twoSum([2,7,11,15], 9))
+print(obj.twoSum([3,2,4], 6))
