fc4e17f74b3d98340a802f6f6b6c7694140dd489
venky3692 <iyervenkatesh92@gmail.com>
2024-11-29 15:58:52 +0530

    29/11

---
+++ Leetcode/Top-150/5.py
@@ -0,0 +1,28 @@
+s = "babad"
+
+res = ""
+resLen = 0
+
+for i in range(len(s)):
+    l, r = i,i
+    print(l)
+    print(r)
+    while l >= 0 and r < len(s) and s[l] == s[r]:
+        print(l)
+        print(r)
+        if (r-l+1) > resLen:
+            res = s[l:r+1]
+            resLen = r - l + 1
+        l-=1
+        r+=1
+    
+    l, r = i, i+1
+    while l>= 0 and r< len(s) and s[l]==s[r]:
+        if (r-l+1) > resLen:
+            res = s[l:r+1]
+            resLen = r - l + 1
+        
+        l-=1
+        r+=1
+
+print(res)
