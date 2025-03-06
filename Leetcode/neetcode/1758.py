ef81b44e702b14c6c580153b33723689d8e238e2
venky3692 <iyervenkatesh92@gmail.com>
2025-03-06 23:50:48 +0530

    06/3

--- Leetcode/neetcode/1758.py
+++ Leetcode/neetcode/1758.py
@@ -3,19 +3,13 @@ s = "1111"
 class Solution:
     def minOperations(self, s):
         count = 0
-        for i in range(len(s)-1):
-            if s[i] == '0':
-                if s[i+1] == '1':
-                    continue
-                else:
-                    count+=1
+        for i in range(len(s)):
+            if i % 2:
+                count += 1 if s[i] == '0' else 0
             else:
-                if s[i+1] == '0':
-                    continue
-                else:
-                    count +=1
+                count +=1 if s[i] == '1' else 0
         
-        return count
+        return min(count, len(s) - count)
     
 obj = Solution()
 print(obj.minOperations(s))
