f4c5caaae8294cffd944ec0b3abc585c0a0155ed
venky3692 <iyervenkatesh92@gmail.com>
2025-03-15 19:20:15 +0530

    15/03

--- Leetcode/neetcode/645.py
+++ Leetcode/neetcode/645.py
@@ -1,10 +1,16 @@
-nums = [1,2,2,4]
+nums = [3,2,2]
 from collections import Counter
 class Solution:
     def findErrorNums(self, nums):
+        l = [0,0]
         so = Counter(nums)
-        mc = so.most_common()
-        print(mc)
+        for i in range(1, len(nums)+1):
+            if so[i] == 0:
+                l[1] = i
+            if so[i] == 2:
+                l[0] = i
+        
+        return l
 
 obj = Solution()
 print(obj.findErrorNums(nums))
