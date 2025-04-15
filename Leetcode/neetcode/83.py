01d2e8207b2952302cd5f9eadd2fbd238c43161c
venky3692 <iyervenkatesh92@gmail.com>
2025-04-15 20:50:54 +0530

    15/4

--- Leetcode/neetcode/83.py
+++ Leetcode/neetcode/83.py
@@ -1,14 +1,10 @@
-def remove_duplicates(self):
-    if self.head is None:
-        return
-    node_values = set()  # set to store unique node values
-    current_node = self.head
-    node_values.add(current_node.value)
-    while current_node.next:
-        if current_node.next.value in node_values:  # duplicate found
-            current_node.next = current_node.next.next
-            self.length -= 1
-        else:
-            node_values.add(current_node.next.value)
-            current_node = current_node.next
-    self.tail = current_node
+class Solution:
+    def deleteDuplicates(self, head):
+        current = head
+        while current:
+            while current.next and current.next.val == current.val:
+                current.next = current.next.next
+            
+            current = current.mext
+        
+        return head
