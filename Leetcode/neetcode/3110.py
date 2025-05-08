24575127d9ac954e3a09baff0d32c1cf9565c78f
venky3692 <iyervenkatesh92@gmail.com>
2025-05-08 20:02:07 +0530

    8/5

---
+++ Leetcode/neetcode/3110.py
@@ -0,0 +1,10 @@
+class Solution:
+    def scoreofString(self, s):
+        sum = 0
+        for i in range(len(s)-1):
+            sum += abs(ord(s[i]) - ord(s[i+1]))
+        
+        return sum
+
+obj = Solution()
+print(obj.scoreofString('zaz'))
