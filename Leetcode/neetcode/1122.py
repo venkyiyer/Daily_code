f6b836824e0c8ae787b45abb31ddb457812b1a8f
venky3692 <iyervenkatesh92@gmail.com>
2025-05-15 20:05:54 +0530

    15/5

--- Leetcode/neetcode/1122.py
+++ Leetcode/neetcode/1122.py
@@ -1,12 +1,22 @@
+from collections import defaultdict
 class Solution:
     def relativeSortArray(self, arr1, arr2):
-        arr = []
-        for i in arr2:
-            for j in arr1:
-                if i == j:
-                    arr.append(j)
+        end = []
+        count_dict = defaultdict(int)
+
+        for i in arr1:
+            if i not in arr2:
+                end.append(i)
+            count_dict[i] +=1
+        end.sort()
+    
+        res = []
+        for n in arr2:
+            for _ in range(count_dict[n]):
+                res.append(n)
         
-        return arr
+        return res + end
+            
 
 obj = Solution()
 print(obj.relativeSortArray(arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]))
