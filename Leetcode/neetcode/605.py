307d2f4dd66cb2e5b39e3ed1b055d073ecd545e0
venky3692 <iyervenkatesh92@gmail.com>
2025-05-19 23:34:14 +0530

    19/5

--- Leetcode/neetcode/605.py
+++ Leetcode/neetcode/605.py
@@ -17,15 +17,3 @@ class Solution:
 
 obj = Solution()
 print(obj.canPlaceFlowers(flowerbed, n))
-
-
-# f = [0] + flowerbed + [0]
-#         for i in range(1,len(f)-1):
-#             if f[i-1] == 0 and f[i] == 0 and f[i+1] ==0:
-#                 f[i] = 1
-#                 n -=1
-        
-#         if n<=0:
-#             return True
-#         else:
-#             return False
