0cac406c14d12f496798ab09f7a2c03d7059c6cf
venky3692 <iyervenkatesh92@gmail.com>
2025-05-28 08:10:05 +0530

    28/5

---
+++ Leetcode/neetcode/238.py
@@ -0,0 +1,17 @@
+class Solution:
+    def productExceptSelf(self, nums):
+        res = [1]* (len(nums))
+
+        prefix =1
+        for i in range(len(nums)):
+            res[i] = prefix 
+            prefix*= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postfix
+            postfix*= nums[i]
+        
+        return res
+
+obj = Solution()
+print(obj.productExceptSelf(nums = [1,2,3,4]))
