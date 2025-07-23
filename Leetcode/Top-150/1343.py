4b8baca1577617490acc1e2304df2aff38f6858a
venky3692 <iyervenkatesh92@gmail.com>
2025-07-23 23:49:10 +0530

    23/7

--- Leetcode/Top-150/1343.py
+++ Leetcode/Top-150/1343.py
@@ -1,11 +1,12 @@
 class Solution:
     def normOfSubarray(self, arr, k, threshold):
         count  = 0
-        for i in range(len(arr)-1):
+        for i in range(len(arr)):
+            print('arr->', arr[i:i+k])
             if len(arr[i:i+k]) == k and sum(arr[i:i+k])//k >= threshold:
                 count += 1
         
         return count
 
 obj = Solution()
-print(obj.normOfSubarray(arr = [1,1,1,1,1], k = 1, threshold = 0))
+print(obj.normOfSubarray(arr = [11,13,17,23,29,31,7,5,2,3], k = 3, threshold = 5))
