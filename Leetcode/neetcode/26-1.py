2cd4d623e6f30a4c4c835a3fccaf793b01442360
venkyiyer <iyervenkatesh92@gmail.com>
2025-09-05 22:59:32 +0530

    5/9

---
+++ Leetcode/neetcode/26-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        k = 0
+        for index in range(1,len(nums)):
+            if(nums[index] != nums[index-1]):
+                k+=1
+                nums[k] = nums[index]
+        return k+1
+
+obj = Solution()
+print(obj.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
