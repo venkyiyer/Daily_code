9a4fb48c476aeb9545f8b83fbfec97997084fd1d
venkyiyer <iyervenkatesh92@gmail.com>
2026-08-25 23:50:19 +0530

    25/8

---
+++ Leetcode/Top-250/344.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseString(self, s):
+        l = 0
+        r = len(s) -1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l +=1
+            r -=1
+        return s
+    
+obj = Solution()
+print(obj.reverseString(["h","e","l","l","o"]))
